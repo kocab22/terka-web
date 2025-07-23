fc5f4ab518497c1af0a108ec6fd13c237e59e26e
kocab22 <kocab22@gmail.com>
2025-07-23 15:14:57 +0200

    ladeni vzhledu, sloupce, responzivita

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,14 @@ const nav = document.getElementById("main-nav");
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  console.log("scrollY:", window.scrollY); // sleduj hodnoty
+  if (window.scrollY > 250) {
+    header.style.opacity = "1";
+  } else {
+    header.style.opacity = "0.5";
+  }
+});
